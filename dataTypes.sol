836570d905e40ffbd19df00f4d416f538bf1fef8
Charles <53397655+charlesncn@users.noreply.github.com>
2022-01-22 13:16:43 +0300

    Create dataTypes.sol

---
+++ dataTypes.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.0;
+
+contract Storage{
+    uint256 faveNum = 67; //unsigned integer of size 256 bits
+    bool name = true;
+    string myToken = "ETH";
+    int256 myInt = -34;
+    address myAdd = 0x353469fgf34797347429744299;
+    bytes32 myByte = "sol";
+    
+}
